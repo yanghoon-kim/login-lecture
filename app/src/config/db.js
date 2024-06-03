f22c594a8a022b48f85269e089de7bf35afa90f2
yanghoon-kim <dongsuh2@hanmail.net>
2024-06-03 20:17:20 +0900

    nodemon-clone후-commit

--- app/src/config/db.js
+++ app/src/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PSWORD,
-  database: process.env.DB_DATABASE,
+  host: 'localhost',
+  user: 'root',
+  password: 'cupid123',
+  insecureAuth: true
 });
 
 db.connect();
